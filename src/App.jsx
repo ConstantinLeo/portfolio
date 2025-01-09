c7423ee9598e8562c1cd3fec65836b15e3551252
Léo Constantin <leo.constantin@etu.umontpellier.fr>
2025-01-09 18:39:01 +0100

    Initialisation du projet React et Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center mt-10">
+                Site en maintenance...
+            </h1>
+        </div>
+    );
+}
+
+export default App
