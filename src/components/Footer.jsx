e607a68a580645a2f1c41cc4e561310edce7482d
Léo Constantin <leo.constantin@etu.umontpellier.fr>
2025-01-20 03:43:08 +0100

    feat: Ajout de l'accès à la page contact dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaFilePdf, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaPhone, FaFilePdf, FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
     return (
@@ -8,7 +8,7 @@ function Footer() {
             <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-6 items-center">
                 {/* Pages du site */}
                 <div className="space-y-2">
-                    <Link to="/" className="block text-slate-gray hover:text-burnt-orange">Accueil</Link>
+                    <Link to="/" className="block text-slate-gray hover:text-burnt-orange">A propos</Link>
                     <Link to="/formation" className="block text-slate-gray hover:text-burnt-orange">Formation</Link>
                     <Link to="/experience" className="block text-slate-gray hover:text-burnt-orange">Expériences</Link>
                     <Link to="/projects" className="block text-slate-gray hover:text-burnt-orange">Projets</Link>
@@ -23,21 +23,28 @@ function Footer() {
                         réservés.</p>
                 </div>
 
-                {/* Réseaux sociaux */}
+                {/* Réseaux sociaux et autres */}
                 <div className="flex justify-center md:justify-end space-x-4">
-                    <a href="/documents/CV-CONSTANTIN-Leo.pdf" target="_blank" rel="noopener noreferrer" className="hover:text-burnt-orange">
-                        <FaFilePdf size={24} />
+                    <a href="/contact" rel="noopener noreferrer"
+                       className="hover:text-burnt-orange">
+                        <FaPhone size={24}/>
                     </a>
-                    <a href="https://linkedin.com/in/leo-constantin" target="_blank" rel="noopener noreferrer" className="hover:text-burnt-orange">
-                        <FaLinkedin size={24} />
+                    <a href="/documents/CV-CONSTANTIN-Leo.pdf" target="_blank" rel="noopener noreferrer"
+                       className="hover:text-burnt-orange">
+                        <FaFilePdf size={24}/>
                     </a>
-                    <a href="https://github.com/ConstantinLeo" target="_blank" rel="noopener noreferrer" className="hover:text-burnt-orange">
-                        <FaGithub size={24} />
+                    <a href="https://linkedin.com/in/leo-constantin" target="_blank" rel="noopener noreferrer"
+                       className="hover:text-burnt-orange">
+                        <FaLinkedin size={24}/>
+                    </a>
+                    <a href="https://github.com/ConstantinLeo" target="_blank" rel="noopener noreferrer"
+                       className="hover:text-burnt-orange">
+                        <FaGithub size={24}/>
                     </a>
                 </div>
             </div>
             <button
-                onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
                 className="fixed bottom-4 right-4 bg-burnt-orange text-white p-2 rounded-full shadow-lg hover:bg-dark-purple focus:outline-none"
             >
                 ↑
