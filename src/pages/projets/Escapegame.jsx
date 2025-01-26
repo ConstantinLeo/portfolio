8c6f43d0a21570f961d2d5f2b40916f2d1aa6f54
Léo Constantin <leo.constantin@etu.umontpellier.fr>
2025-01-26 20:12:17 +0100

    feat: Ajout du contenu pour l'Escape Game

---
+++ src/pages/projets/Escapegame.jsx
@@ -0,0 +1,53 @@
+function Escapegame() {
+    return (
+        <div className="font-poppins py-12 bg-white-smoke min-h-screen">
+            <div className="container mx-auto px-4">
+                <h1 className="text-3xl font-bold text-dark-purple mb-6">
+                    Escape Game
+                </h1>
+                <p className="text-lg text-slate-gray mb-4">
+                    Durant la première année de BUT Informatique, j'ai été amené à imaginer un Escape Game avec mon
+                    groupe, en élaborant une affiche de communication pour celui-ci pour commencer.
+                </p>
+                <p className="text-lg text-slate-gray mb-6">
+                    Par la suite, nous devions concevoir le site Web de l'Escape Game qu'un autre groupe avait réfléchi,
+                    afin d'observer si les exigences étaient bien accomplies.
+                </p>
+                <h2 className="text-2xl font-semibold text-dark-purple mb-4">
+                    Explication du jeu développé
+                </h2>
+                <p className="text-lg text-slate-gray mb-4">
+                    Le projet a débuté par une demande spécifique du groupe qui souhaitait organiser un escape game dans un opéra, avec des missions précises à accomplir.
+                    Afin de bien comprendre leurs attentes, nous avons pris contact directement avec eux pour recueillir le plus d'informations possible sur le concept qu'ils souhaitaient.
+                    L'objectif était de créer une expérience immersive et originale, où les joueurs seraient plongés dans l'univers de l'opéra tout en accomplissant des défis.
+                </p>
+                <p className="text-lg text-slate-gray mb-4">
+                    À partir de cette base, notre groupe a réalisé un site Web pour présenter l'Escape Game qu'ils avaient réfléchi.
+                    Ce site met en avant l'expérience immersive de l'escape game, en détaillant les différentes salles et missions imaginées pour l'opéra.
+                    Il illustre également l'ambiance unique du jeu et guide les futurs participants dans leur aventure.
+                </p>
+                <div className="mb-6">
+                    <img
+                        src=".././images/site-escapegame.jpg"
+                        alt="[IMAGE SITE ESCAPE GAME]"
+                        className="rounded-lg w-full h-auto object-contain"
+                    />
+                </div>
+                <h2 className="text-2xl font-semibold text-dark-purple mb-4">
+                    Ce que ce projet m’a apporté
+                </h2>
+                <p className="text-lg text-slate-gray mb-4">
+                    Ce projet m'a fait prendre conscience de l'intérêt d'un bon reccueil des exigences auprès du client, avant de démarrer un projet.
+                    Effectivement, sans savoir quelle direction prendre, je considère qu'il est impossible d'avancer. Alors j'ai mis cette compétence au profit de mes expériences professionnelles par la suite.
+                </p>
+                <p className="text-lg text-slate-gray mb-4">
+                    Aussi, j'ai observé que le travail d'équipe est essentiel dans les projets, qu'ils soient basés sur l'informatique ou non.
+                    C'est une compétence solide que j'ai acquis et que j'utilise désormais dans n'importe quelle tâche devant
+                    être effectuée à l'unisson.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Escapegame;
